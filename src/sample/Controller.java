5f92f2add13cb8c7d002d8087bd85787166403f3
clam3154 <clam3154@bths.edu>
2019-01-03 09:29:01 -0500

    ...

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,13 +3,14 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
-public class Controller {
-    public Label helloWorld;
+public class Controller
+{
+    public Label score;
     private int count = 0;
 
-    public void Count(ActionEvent actionEvent)
+    public void pressButton(ActionEvent actionEvent)
     {
         count++;
-        helloWorld.setText("Score: " + String.valueOf(count));
+        score.setText("Score: " + String.valueOf(count));
     }
 }
